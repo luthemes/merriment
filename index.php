c7016d09b53e20d628da487e0b850026196e2bea
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-11-19 23:40:35 +0000

    new beginning

--- index.php
+++ index.php
@@ -1,33 +1,11 @@
 <?php
-/*
-================================================================================================
-Merriment - index.php
-================================================================================================
-This is the most generic template file in a WordPress theme and is one of the two required files 
-for a theme (the other being style.css). The index.php template file is flexible. It can be used to 
-include all references to the header, content, widget, footer and any other pages created in 
-WordPress. Or it can be divided into modular template files, each taking on part of the workload. 
-If you do not provide other template files, WordPress may have default files or functions to 
-perform their jobs.
+/**
+ * Index template.
+ *
+ * Loading this template on a website should be avoided, unless a plugin is misbehaving
+ * or there is an abnormal situation. However, as a fallback measure in case this file
+ * is loaded, the following code will correctly load our `public/view/index.php` template.
+ */
 
-@package        Merriment WordPress Theme
-@copyright      Copyright (C) 2017. Benjamin Lu
-@license        GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
-@author         Benjamin Lu (https://www.benjlu.com/)
-================================================================================================
-*/
-?>
-<?php get_header(); ?>
-    <div id="site-main" class="site-main">
-        <div id="content-area" class="content-area">
-            <?php if (have_posts()) : ?>
-                <?php while (have_posts()) : the_post(); ?>
-                    <?php get_template_part('template-parts/content', get_post_format()); ?>
-            <?php endwhile; ?>
-                <?php the_posts_pagination(); ?>
-            <?php else : ?>
-                    <?php get_template_part('template-parts/content', 'none'); ?>
-            <?php endif; ?>
-        </div>
-    </div>
-<?php get_footer(); ?>
+// Access the view template engine.
+Backdrop\View\display( 'index' );
