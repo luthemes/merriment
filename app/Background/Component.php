0243b7145b13d80f9db929236275efdd752296b4
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-11-21 17:33:22 +0000

    initial commit

---
+++ app/Background/Component.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Custom Backdround Component.
+ *
+ * Manages the custom header component.
+ *
+ * @package   Merriment
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/merriment
+ */
+
+namespace Merriment\Background;
+
+use Backdrop\Contracts\Bootable;
+
+/**
+ * Custom Header Component class.
+ *
+ * @since  0.0.1
+ * @access public
+ */
+class Component implements Bootable {
+
+	/**
+	 * Bootstraps the component.
+	 *
+	 * @since  0.0.1
+	 * @access public
+	 * @return void
+	 */
+	public function boot(): void {
+
+		// Register headers on `after_setup_theme`.
+		add_action( 'after_setup_theme', [ $this, 'register' ] );
+	}
+
+	/**
+	 * Runs the register actions.
+	 *
+	 * @since  0.0.1
+	 * @access public
+	 * @return void
+	 */
+	public function register() {
+        $defaults = array(
+            'default-color'          => '',
+            'default-image'          => get_parent_theme_file_uri( 'public/images/background.jpg' ),
+            'default-repeat'         => 'repeat',
+            'default-position-x'     => 'left',
+            'default-position-y'     => 'top',
+            'default-size'           => 'auto',
+            'default-attachment'     => 'scroll',
+        );
+        add_theme_support( 'custom-background', $defaults );
+	}
+}
