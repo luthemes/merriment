3c8a33551a3ec23e6f7d5bd1595e136eab2009bc
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-11-27 19:03:30 +0000

    initial commit

---
+++ config/_settings-menus.php
@@ -0,0 +1,23 @@
+
+<?php
+/**
+ * Menu Settings Configuration.
+ *
+ * This configuration file defines the menu locations for the Merriment theme.
+ * Each menu location includes a name and is registered during theme setup.
+ *
+ * @package   Merriment
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/merriment
+ */
+
+return [
+    'primary' => [
+        'name' => __( 'Primary Navigation', 'merriment' ),
+    ],
+    'social' => [
+        'name' => __( 'Social Navigation', 'merriment' ),
+    ]
+];
