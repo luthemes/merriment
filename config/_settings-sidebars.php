d8fdfd1699e4e230e0c58cd8c740976dde501457
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-12-03 00:41:45 +0000

    initial commit

---
+++ config/_settings-sidebars.php
@@ -0,0 +1,26 @@
+
+<?php
+/**
+ * Sidebar Settings Configuration.
+ *
+ * This file defines the sidebar locations for the Merriment theme.
+ * These settings are used during theme initialization to register
+ * the available sidebars with their respective names and descriptions.
+ *
+ * @package   Merriment
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/merriment
+ */
+
+return [
+    'primary' => [
+        'name'        => 'Primary Sidebar',
+        'description' => 'The main sidebar for widgets alongside primary content.'
+    ],
+    'footer' => [
+        'name'        => 'Footer Sidebar',
+        'description' => 'A widget area displayed in the footer section of the site.'
+    ],
+];
